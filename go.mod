c4bab0d98cebb4d71edcd7af8837aa0fa708082b
hoanthien <hoanthien97@gmail.com>
2025-01-19 12:07:03 +0700

    First interpreter that can interpret bf code

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module brain-fuck
 
 go 1.23.3
+
+require (
+	github.com/hoanthiennguyen/go-stack v0.0.1
+)
--- main.go
+++ main.go
@@ -1,40 +1,79 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
 	"os"
-	"strings"
+
+	"github.com/hoanthiennguyen/go-stack"
 )
 
-func main() {
-	fmt.Println("Welcome to Go REPL. Type 'exit()' to quit.")
-
-	reader := bufio.NewReader(os.Stdin)
-	lines := []string{}
-	fmt.Print(">> ") // Prompt
-	for {
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
+func execute(srcCode string) {
+	memory := make([]int, 30000)
+	srcCounter := 0
+	dataPointer := 0
+	// Store parathensis position
+	stk := stack.New[int]()
+	var err error
+	for srcCounter < len(srcCode) {
+		instruction := rune(srcCode[srcCounter])
+		switch instruction {
+		case '+':
+			memory[dataPointer] += 1
+			srcCounter++
+		case '-':
+			memory[dataPointer] -= 1
+			srcCounter++
+		case '>':
+			dataPointer++
+			srcCounter++
+		case '<':
+			if dataPointer > 0 {
+				dataPointer--
+			}
+			srcCounter++
+		case '.':
+			data := rune(memory[dataPointer])
+			fmt.Print(string(data))
+			srcCounter++
+		case ',':
+			input := int('i')
+			memory[dataPointer] = input
+			srcCounter++
+			// TODO
+		case '[':
+			if memory[dataPointer] > 0 {
+				stk.Push(srcCounter)
+				srcCounter++
+			} else {
+				for srcCode[srcCounter] != ']' {
+					srcCounter++
+				}
+				srcCounter++
+			}
+		case ']':
+			if stk.IsEmpty() {
+				panic("Stack is empty")
+			}
+			srcCounter, err = stk.Pop()
+			if err != nil {
+				panic(err)
+			}
+		default:
+			srcCounter++
 		}
-
-		input = strings.TrimSpace(input)
-		if input == "exit()" {
-			break
-		}
-
-		lines = append(lines, input)
-		if strings.HasSuffix(input, ";") {
-			output := strings.Join(lines, "\n")
-			fmt.Println(output)
-			lines = []string{}
-			fmt.Print(">> ") // Prompt
-		} else {
-			// continue to read input
-			fmt.Print("> ")
-		}
-
 	}
 }
+
+func main() {
+	fileName := os.Args[1]
+	contentRaw, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	execute(string(contentRaw))
+}
+
+// func isValidChar(c rune) bool {
+// 	return slices.Contains([]rune{'+', '-', '>', '<', '.', '[', ']', ','}, c)
+// }
