811a99e39534c5151152d555c2432b5d8487e400
hoanthien <hoanthien97@gmail.com>
2025-03-12 22:07:11 +0700

    Add some optimzation: precompute open-close brackets

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"os"
 	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/hoanthiennguyen/go-stack"
 )
 
-func execute(srcCode string) {
+func execute(srcCode string, correspondingClosingBracket map[int]int) {
 	memory := make([]int, 1000000)
 	srcCounter := 0
 	dataPointer := 0
@@ -40,20 +42,7 @@ func execute(srcCode string) {
 			if memory[dataPointer] > 0 {
 				stk.Push(srcCounter)
 			} else {
-				dept := 1
-				for dept > 0 {
-					srcCounter++
-					if srcCounter == len(srcCode) {
-						panic("Unmatched [")
-					}
-
-					if srcCode[srcCounter] == ']' {
-						dept--
-					}
-					if srcCode[srcCounter] == '[' {
-						dept++
-					}
-				}
+				srcCounter = correspondingClosingBracket[srcCounter]
 			}
 		case ']':
 			if memory[dataPointer] > 0 {
@@ -87,14 +76,87 @@ func filterComment(srcCode string) string {
 }
 
 func main() {
-	fileName := os.Args[1]
+	f1 := os.Args[1]
+	switch f1 {
+	case "compile":
+		compile(os.Args[2])
+	case "run":
+		run(os.Args[2])
+	}
+}
+
+func run(compiledFileName string) {
+	contentRaw, err := os.ReadFile(compiledFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	arr := strings.Split(string(contentRaw), "\n")
+	firstLine := arr[0]
+	srcCode := arr[1]
+
+	arr = strings.Split(firstLine, ",")
+	closingBracket := make(map[int]int)
+	for index := 0; index < len(arr); index += 2 {
+		open, _ := strconv.Atoi(arr[index])
+		closing, _ := strconv.Atoi(arr[index+1])
+		closingBracket[open] = closing
+	}
+
+	execute(srcCode, closingBracket)
+}
+
+func buildClosingBracketMap(srcCode string) [][2]int {
+	closingBracket := [][2]int{}
+	for i := 0; i < len(srcCode); i++ {
+		instruction := rune(srcCode[i])
+		if instruction == '[' {
+			tmp := i + 1
+			countMissingBracket := 1
+			for tmp < len(srcCode) {
+				if rune(srcCode[tmp]) == ']' {
+					countMissingBracket--
+					if countMissingBracket == 0 {
+						break
+					}
+
+				} else if rune(srcCode[tmp]) == '[' {
+					countMissingBracket++
+				}
+
+				tmp++
+			}
+
+			closingBracket = append(closingBracket, [2]int{i, tmp})
+		}
+	}
+
+	return closingBracket
+}
+
+func compile(fileName string) {
 	contentRaw, err := os.ReadFile(fileName)
 	if err != nil {
 		panic(err)
 	}
 
-	content := filterComment(string(contentRaw))
-	execute(content)
+	srcCode := filterComment(string(contentRaw))
+
+	closingBracketsMap := buildClosingBracketMap(srcCode)
+	arr := []string{}
+	for _, e := range closingBracketsMap {
+		arr = append(arr, fmt.Sprint(e[0]), fmt.Sprint(e[1]))
+	}
+
+	serializeString := strings.Join(arr, ",")
+	serializeString = serializeString + "\n" + srcCode
+
+	nameArr := strings.Split(fileName, ".")
+	fileNameCompiled := nameArr[0] + ".cpl"
+	if err := os.WriteFile(fileNameCompiled, []byte(serializeString), os.ModePerm); err != nil {
+		panic(err)
+	}
+	fmt.Printf("Compiled successfully %s\n", fileNameCompiled)
 }
 
 //	func isValidChar(c rune) bool {
